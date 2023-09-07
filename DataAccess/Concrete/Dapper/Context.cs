4ea8a7730ba1af03b86f391f554952c9fae18c47
ErayBayrak <47663033+ErayBayrak@users.noreply.github.com>
2023-09-07 21:46:42 +0300

    context updated

---
+++ DataAccess/Concrete/Dapper/Context.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.Dapper
+{
+    public class Context
+    {
+        private readonly IConfiguration _configuration;
+        private readonly string _connectionString;
+        public Context(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _connectionString = _configuration.GetConnectionString("connection");
+        }
+        public IDbConnection CreateConnection() => new SqlConnection(_connectionString);
+    }
+}
