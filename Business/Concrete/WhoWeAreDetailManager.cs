91188dcc40048d93db1f5ea2c775f8aac60773ff
ErayBayrak <47663033+ErayBayrak@users.noreply.github.com>
2023-09-21 22:06:51 +0300

    whowearedetail service

--- Business/Concrete/WhoWeAreDetailManager.cs
+++ Business/Concrete/WhoWeAreDetailManager.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Business.Abstract;
+using Core.DTOs.WhoWeAreDetail;
+using DataAccess.Abstract;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,83 @@ using System.Threading.Tasks;
 
 namespace Business.Concrete
 {
-    public class WhoWeAreDetailManager
+    public class WhoWeAreDetailManager : IWhoWeAreDetailService
     {
+        IWhoWeAreDetailDal _whoWeAreDetailDal;
+
+        public WhoWeAreDetailManager(IWhoWeAreDetailDal whoWeAreDetailDal)
+        {
+            _whoWeAreDetailDal = whoWeAreDetailDal;
+        }
+
+        public void Add(CreateWhoWeAreDetailDto dto)
+        {
+            try
+            {
+                _whoWeAreDetailDal.Add(dto);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+
+        public void Delete(int id)
+        {
+            try
+            {
+                _whoWeAreDetailDal.Delete(id);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+
+        public Task<List<ResultWhoWeAreDetailDto>> GetAll()
+        {
+            try
+            {
+                return _whoWeAreDetailDal.GetAll();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+
+        public Task<ResultWhoWeAreDetailDto> GetById(int id)
+        {
+            try
+            {
+                return _whoWeAreDetailDal.GetById(id);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+
+        public void Update(UpdateWhoWeAreDetailDto dto)
+        {
+            try
+            {
+                _whoWeAreDetailDal.Update(dto);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
     }
 }
