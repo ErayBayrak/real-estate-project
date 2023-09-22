baaee6f6a34717fa2865ed292e98ff9bbbf11845
ErayBayrak <47663033+ErayBayrak@users.noreply.github.com>
2023-09-22 21:04:12 +0300

    resultproductdto updated

--- Core/DTOs/Product/ResultProductDto.cs
+++ Core/DTOs/Product/ResultProductDto.cs
@@ -13,8 +13,6 @@ namespace Core.DTOs.Product
         public decimal Price { get; set; }
         public string City { get; set; }
         public string District { get; set; }
-        public string Type { get; set; }
-        public string Address { get; set; }
         public int CategoryId { get; set; }
     }
 }
