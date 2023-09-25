46ad017f7a6ff6326e1f1370982f5a98e0ae82d7
ErayBayrak <47663033+ErayBayrak@users.noreply.github.com>
2023-09-25 12:34:29 +0300

    resultservicedto created

---
+++ DataAccess/Abstract/IServiceDal.cs
@@ -0,0 +1,18 @@
+﻿using Core.DTOs.Category;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+    public interface IServiceDal
+    {
+        Task<List<ResultCategoryDto>> GetAll();
+        void Add(CreateCategoryDto categoryDto);
+        void Delete(int id);
+        void Update(UpdateCategoryDto categoryDto);
+        Task<ResultCategoryDto> GetById(int id);
+    }
+}
