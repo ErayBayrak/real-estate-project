804ae841b33d75e5a5d5325c71ff1ca9a190a2c6
ErayBayrak <47663033+ErayBayrak@users.noreply.github.com>
2023-09-25 12:52:43 +0300

    iserviceservice

---
+++ Business/Abstract/IServiceService.cs
@@ -0,0 +1,19 @@
+﻿using Core.DTOs.Category;
+using Core.DTOs.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstract
+{
+    public interface IServiceService
+    {
+        Task<List<ResultServiceDto>> GetAll();
+        void Add(CreateServiceDto dto);
+        void Delete(int id);
+        void Update(UpdateServiceDto dto);
+        Task<ResultServiceDto> GetById(int id);
+    }
+}
