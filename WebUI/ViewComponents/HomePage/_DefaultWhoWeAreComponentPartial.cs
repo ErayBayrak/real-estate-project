053ced1f76ea595bb8390177b377d2b6623b171a
ErayBayrak <47663033+ErayBayrak@users.noreply.github.com>
2023-09-25 13:40:37 +0300

    dynamic datas of services listed in ui

--- WebUI/ViewComponents/HomePage/_DefaultWhoWeAreComponentPartial.cs
+++ WebUI/ViewComponents/HomePage/_DefaultWhoWeAreComponentPartial.cs
@@ -15,16 +15,25 @@ namespace WebUI.ViewComponents.HomePage
         public async Task<IViewComponentResult> InvokeAsync()
         {
             var client = _httpClientFactory.CreateClient();
+            var clientService = _httpClientFactory.CreateClient();
+
             var responseMessage = await client.GetAsync("http://localhost:13838/api/WhoWeAreDetail/getalldetails");
-            if (responseMessage.IsSuccessStatusCode)
+            var responseMessageService = await clientService.GetAsync("http://localhost:13838/api/Service");
+
+
+            if (responseMessage.IsSuccessStatusCode && responseMessageService.IsSuccessStatusCode)
             {
                 var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var jsonDataService = await responseMessageService.Content.ReadAsStringAsync();
+
                 var values = JsonConvert.DeserializeObject<List<ResultWhoWeAreDetailDto>>(jsonData);
+                var valuesService = JsonConvert.DeserializeObject<List<ResultServiceDto>>(jsonDataService);
+
                 ViewBag.Title = values.Select(x => x.Title).FirstOrDefault();
                 ViewBag.Subtitle = values.Select(x => x.Subtitle).FirstOrDefault();
                 ViewBag.Description1 = values.Select(x => x.Description1).FirstOrDefault();
                 ViewBag.Description2 = values.Select(x => x.Description2).FirstOrDefault();
-                return View(values);
+                return View(valuesService);
             }
             return View();
         }
