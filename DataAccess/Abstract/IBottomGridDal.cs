ed6668c63b7b0d13fe715e7ecdc7f9445bf214a3
ErayBayrak <47663033+ErayBayrak@users.noreply.github.com>
2023-09-26 13:06:58 +0300

    bottomgrid dtos

---
+++ DataAccess/Abstract/IBottomGridDal.cs
@@ -0,0 +1,18 @@
+﻿using Core.DTOs.BottomGrid;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+    public interface IBottomGridDal
+    {
+        Task<List<ResultBottomGridDto>> GetAll();
+        void Add(CreateBottomGridDto dto);
+        void Delete(int id);
+        void Update(UpdateBottomGridDto dto);
+        Task<ResultBottomGridDto> GetById(int id);
+    }
+}
