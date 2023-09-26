2de10e5740e81bf109f74449a0436454d1b6787c
ErayBayrak <47663033+ErayBayrak@users.noreply.github.com>
2023-09-26 21:04:04 +0300

    popular location service

---
+++ Business/Abstract/IPopularLocationService.cs
@@ -0,0 +1,19 @@
+﻿using Core.DTOs.BottomGrid;
+using Core.DTOs.PopularLocation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstract
+{
+    public interface IPopularLocationService
+    {
+        Task<List<ResultPopularLocationDto>> GetAll();
+        void Add(CreatePopularLocationDto dto);
+        void Delete(int id);
+        void Update(UpdatePopularLocationDto dto);
+        Task<ResultPopularLocationDto> GetById(int id);
+    }
+}
