6b7be67450162036599a0a05bc9250bfd8784935
ErayBayrak <47663033+ErayBayrak@users.noreply.github.com>
2023-09-26 13:47:34 +0300

    bottom grid interface of service

---
+++ Business/Abstract/IBottomGridService.cs
@@ -0,0 +1,19 @@
+﻿using Core.DTOs.BottomGrid;
+using Core.DTOs.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstract
+{
+    public interface IBottomGridService
+    {
+        Task<List<ResultBottomGridDto>> GetAll();
+        void Add(CreateBottomGridDto dto);
+        void Delete(int id);
+        void Update(UpdateBottomGridDto dto);
+        Task<ResultBottomGridDto> GetById(int id);
+    }
+}
