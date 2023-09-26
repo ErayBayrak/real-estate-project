d2cdfd4d417bcd2636d979a51a17179f874c16a7
ErayBayrak <47663033+ErayBayrak@users.noreply.github.com>
2023-09-26 20:50:38 +0300

    popular location service

---
+++ DataAccess/Abstract/IPopularLocation.cs
@@ -0,0 +1,18 @@
+﻿using Core.DTOs.PopularLocation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+    public interface IPopularLocation
+    {
+        Task<List<ResultPopularLocationDto>> GetAll();
+        Task<ResultPopularLocationDto> GetById(int id);
+        void Add(CreatePopularLocationDto dto);
+        void Update(UpdatePopularLocationDto dto);
+        void Delete(int id);
+    }
+}
