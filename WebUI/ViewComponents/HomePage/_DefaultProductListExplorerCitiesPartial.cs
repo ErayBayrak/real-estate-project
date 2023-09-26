8c99e5ede1c8fc069b0a56608e44f7bfb41bc224
ErayBayrak <47663033+ErayBayrak@users.noreply.github.com>
2023-09-26 21:18:56 +0300

    popular location partial consumed

--- WebUI/ViewComponents/HomePage/_DefaultProductListExplorerCitiesPartial.cs
+++ WebUI/ViewComponents/HomePage/_DefaultProductListExplorerCitiesPartial.cs
@@ -1,11 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using WebUI.DTOs.PopularLocation;
 
 namespace WebUI.ViewComponents.HomePage
 {
     public class _DefaultProductListExplorerCitiesPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DefaultProductListExplorerCitiesPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("http://localhost:13838/api/PopularLocation");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultPopularLocationDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
