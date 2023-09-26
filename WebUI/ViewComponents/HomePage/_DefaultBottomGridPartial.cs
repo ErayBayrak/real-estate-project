2693e3e8d36fe2bae5d179373c471c61c05e3ffa
ErayBayrak <47663033+ErayBayrak@users.noreply.github.com>
2023-09-26 14:11:45 +0300

    ui

---
+++ WebUI/ViewComponents/HomePage/_DefaultBottomGridPartial.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using WebUI.DTOs.BottomGrid;
+
+namespace WebUI.ViewComponents.HomePage
+{
+    public class _DefaultBottomGridPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DefaultBottomGridPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("http://localhost:13838/api/BottomGrid");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultBottomGridDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
