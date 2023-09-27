ac03eed5d0a65c0dbc1cd57ac3f0c145b666c52b
ErayBayrak <47663033+ErayBayrak@users.noreply.github.com>
2023-09-27 23:13:27 +0300

    testimonial api controller

---
+++ WebAPI/Controllers/TestimonialController.cs
@@ -0,0 +1,24 @@
+﻿using Business.Abstract;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestimonialController : ControllerBase
+    {
+        ITestimonialService _testimonialService;
+
+        public TestimonialController(ITestimonialService testimonialService)
+        {
+            _testimonialService = testimonialService;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var values = await _testimonialService.GetAll();
+            return Ok(values);
+        }
+    }
+}
