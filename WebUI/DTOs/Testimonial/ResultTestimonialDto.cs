4b766fb9ddbf31c6c82c364f9263365acfed16e7
ErayBayrak <47663033+ErayBayrak@users.noreply.github.com>
2023-09-27 23:18:51 +0300

    testimonial ui

---
+++ WebUI/DTOs/Testimonial/ResultTestimonialDto.cs
@@ -0,0 +1,9 @@
+﻿namespace WebUI.DTOs.Testimonial
+{
+    public class ResultTestimonialDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Comment { get; set; }
+    }
+}
