ebe200733b6ca1fb160a6f9738c9a3c968112c12
ErayBayrak <47663033+ErayBayrak@users.noreply.github.com>
2023-09-27 22:45:40 +0300

    ui partial classes

---
+++ WebUI/ViewComponents/HomePage/_DefaultDiscountOfDayComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebUI.ViewComponents.HomePage
+{
+    public class _DefaultDiscountOfDayComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
