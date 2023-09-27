93232f25d0ef0aeca8e8c6a6bac7aa37c04b2eed
ErayBayrak <47663033+ErayBayrak@users.noreply.github.com>
2023-09-27 23:11:38 +0300

    testimonial service

---
+++ Business/Concrete/TestimonialManager.cs
@@ -0,0 +1,36 @@
+﻿using Business.Abstract;
+using Core.DTOs.Testimonial;
+using DataAccess.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class TestimonialManager : ITestimonialService
+    {
+        ITestimonialDal _testimonialDal;
+
+        public TestimonialManager(ITestimonialDal testimonialDal)
+        {
+            _testimonialDal = testimonialDal;
+        }
+
+        public Task<List<ResultTestimonialDto>> GetAll()
+        {
+            try
+            {
+                var values = _testimonialDal.GetAll();
+                return values;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+             
+        }
+    }
+}
