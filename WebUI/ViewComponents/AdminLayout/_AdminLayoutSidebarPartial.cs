b370d64b711fd4adedd9475a8dabc2dac61fd171
ErayBayrak <47663033+ErayBayrak@users.noreply.github.com>
2023-10-05 14:02:01 +0300

    layout

---
+++ WebUI/ViewComponents/AdminLayout/_AdminLayoutSidebarPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebUI.ViewComponents.AdminLayout
+{
+    public class _AdminLayoutSidebarPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ WebUI/ViewComponents/AdminLayout/_AdminLayoutSpinnerPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebUI.ViewComponents.AdminLayout
+{
+    public class _AdminLayoutSpinnerPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
